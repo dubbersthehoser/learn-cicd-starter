652db120887e2426858599c7d972ec01cef0449c
bf <bf@null.local>
2025-10-15 10:50:44 -0700

    actual test for get header api key.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		input       map[string]string
+		expect      string
+		shouldError bool
+	}
+
+	cases := []test{
+		test{
+			input: map[string]string{
+				"Authorization": "ApiKey lol",
+			},
+			expect: "lol",
+		},
+		test{
+			input: map[string]string{
+				"Authorization": "ApiKey",
+			},
+			expect: "",
+			shouldError: true,
+		},
+		test{
+			input: map[string]string{
+				"noAuth": "wop wop...",
+			},
+			expect: "",
+			shouldError: false,
+		},
+	}
+
+
+	for i, _case := range cases {
+		header := http.Header{}
+		for k, v := range _case.input {
+			header.Set(k, v)
+		}
+		expect := _case.expect
+		actual, err := GetAPIKey(header)
+		if _case.shouldError {
+			if err == nil {
+				t.Errorf("case %d, expected error, got nil", i)
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("case %d, unexpected error: %s", i, err)
+			continue
+		}
+
+		if actual != expect {
+			t.Errorf("case %d, expect %s, got %s", i, expect, actual)
+		}
+	}
+}
